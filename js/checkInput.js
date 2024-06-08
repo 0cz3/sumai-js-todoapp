672a19f766553f5a3307821c5656d0dc807e3466
0cz <sumire.hamaguchi@nijibox.co.jp>
2024-06-08 21:07:34 +0900

    submitボタンの活性スタイルをクラスで定義

--- js/checkInput.js
+++ js/checkInput.js
@@ -1,17 +1,23 @@
 const input = document.querySelector('.js_addTodo_input');
 const submitButton = document.querySelector('.js_addTodo_submit');
+const IS_INACTIVE = 'is-inactive';
 
 /**
  * 入力内容が空欄の場合はsubmitButtonを無効化
  */
 const checkInput = () => {
+  const submitHasInactive = submitButton.classList.contains(IS_INACTIVE);
   if (/^\S/.test(input.value)) {
     submitButton.disabled = false;
-    submitButton.style.opacity = 1;
+    if (submitHasInactive) {
+      submitButton.classList.remove(IS_INACTIVE);
+    }
     return;
   }
   submitButton.disabled = true;
-  submitButton.style.opacity = 0.5;
+  if (!submitHasInactive) {
+    submitButton.classList.add(IS_INACTIVE);
+  }
 };
 
 export default checkInput;

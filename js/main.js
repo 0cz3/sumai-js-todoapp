2c7dd808764e1425139512f817b692e74203a452
濱口 菫 <sumire_h2@waku-2.com>
2024-08-03 14:10:58 +0900

    refactor: querySelectorで取得した要素の説明を追記

--- js/main.js
+++ js/main.js
@@ -2,11 +2,35 @@ import { Todo } from './modules/todo.js';
 import { getStorageTodoTasks } from './modules/localStorage.js';
 import toggleSubmitActive from './modules/toggleSubmitActive.js';
 import dropdown from './modules/dropdown.js';
-const submitButton = document.querySelector('.js_addTodo_submit');
-const todoTaskList = document.querySelector('.js_todoTask_list');
+/**
+ * TODOタスク入力フォーム
+ * @type {HTMLFormElement | null}
+ */
 const inputForm = document.querySelector('.js_addTodo_form');
+/**
+ * inputFormのTODOタスク名入力欄
+ * @type {HTMLInputElement | null}
+ */
 const inputField = document.querySelector('.js_addTodo_input');
+/**
+ * inputFormのTODOタスク期限入力欄
+ * @type {HTMLInputElement | null}
+ */
 const inputDate = document.querySelector('.js_addTodo_inputDate');
+/**
+ * inputFormの送信ボタン
+ * @type {HTMLInputElement | null}
+ */
+const submitButton = document.querySelector('.js_addTodo_submit');
+/**
+ * 追加されたTODOタスクをまとめたulリスト
+ * @type {HTMLUListElement | null}
+ */
+const todoTaskList = document.querySelector('.js_todoTask_list');
+/**
+ * todoTaskListの表示切り替えをボタン
+ * @type {HTMLUListElement | null}
+ */
 const toggleButton = document.querySelector('.js_todoTask_toggle');
 /**
  * ストレージからタスクの情報を呼び出して反映

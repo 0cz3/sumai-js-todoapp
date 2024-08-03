7485154d66815e19e5845a4b3d653738f7e75d68
濱口 菫 <sumire_h2@waku-2.com>
2024-08-03 14:38:13 +0900

    refactor: dropdown.jsのJSDocなど修正

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,14 +1,13 @@
 import { IS_CLOSED } from '../constants.js';
-
 /**
- * todoタスクの表示非表示切り替え
- * @param {HTMLButtonElement} toggleButton
- * @param {HTMLButtonElement} todoTaskList
- * @return {void}
+ * toggleButtonクリックでtoggleButtonの状態とtodoTaskListの表示非表示切り替え
+ * @function
+ * @param {Element|null} toggleButton
+ * @param {Element|null} todoTaskList
  */
 const dropdown = (toggleButton, todoTaskList) => {
-  toggleButton.addEventListener('click', ({currentTarget}) => {
-    currentTarget.classList.toggle(IS_CLOSED);
+  toggleButton.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle(IS_CLOSED);
     todoTaskList.classList.toggle(IS_CLOSED);
   });
 };

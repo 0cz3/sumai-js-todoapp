dc5d5308f02bd53c99ebbc54f72cbe2b7d475604
濱口 菫 <sumire_h2@waku-2.com>
2024-08-03 15:18:19 +0900

    refactor: localStorage.jsへJSDocを追加

--- js/modules/localStorage.js
+++ js/modules/localStorage.js
@@ -1,10 +1,21 @@
 import { STORAGE_TODO_TASKS } from '../constants.js';
-
-export const setStorageTodoTasks = (todoTasks) => {
-  localStorage.setItem(STORAGE_TODO_TASKS, JSON.stringify(todoTasks));
+/**
+ * itemsをローカルストレージへ保存
+ * @function
+ * @param {Todo[]} items Todoオブジェクトが格納された配列 || 空配列
+ */
+export const setStorageTodoTasks = (items) => {
+  localStorage.setItem(STORAGE_TODO_TASKS, JSON.stringify(items));
 };
-
+/**
+ * ローカルストレージから呼び出し
+ * @returns {Todo[]} Todoオブジェクトが格納された配列 || 空配列
+ */
 export const getStorageTodoTasks = () => {
-  const getItems = localStorage.getItem(STORAGE_TODO_TASKS);
-  return JSON.parse(getItems) || [];
+  /**
+   * ローカルストレージの保存内容
+   * @type {string || null} JSON化されたTodoオブジェクト || null
+   */
+  const items = localStorage.getItem(STORAGE_TODO_TASKS);
+  return JSON.parse(items) || [];
 };
